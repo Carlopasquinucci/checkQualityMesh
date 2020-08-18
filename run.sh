ff716ace3c441dfc798e0de913cb2e301af4e366
Carlopasquinucci <you@example.com>
2020-08-18 10:10:51 +0200

    Max ortho value and max skew

--- run.sh
+++ run.sh
@@ -1,47 +1,71 @@
 
+version=1
+subversion=0
+file=checkMesh-nonortho.log
 
 #open checkMesh and extraplote singnificant data
+if [[ $1 == "-help" ]]; then
+	echo "Version $version . $subversion"
+	echo "-version for version number"
+	echo "-help to run the help"
+	echo "-checkMesh to run the checkMesh"
+	echo "-checkMesh -parallel nprocessor to run the checkMesh in parallel"
+exit 1
+fi
 
 
-#checkMesh > checkMesh.log
+if [[ $1 == "-version" ]]; then
+	echo "Version $version . $subversion"
+exit 1
+fi
 
-#number of cells --- not robust
-#head -n 33 checkMesh.log > head.log
-#ncells=$(tail -n 1 head.log | cut -d ":" -f 2)
-#
+if [[ $1 == "-checkMesh" ]]; then
+	checkMesh > checkMesh.log
+	echo "Mesh checking"
+fi
 
-#echo($ncells)
+if [[ $2 == "-parallel" ]]; then
+	mpirun -np $3 checkMesh > checkMesh-nonortho.log.log
+	echo "Mesh checking in parallel"
+fi
 
-grep "cells:" checkMesh.log > ncells.log
+# Number of cells
+grep "cells:" $file > ncells.log
 ncells=$(awk '{print $NF}' ncells.log)
 echo $ncells
 
 
-
 # Mesh ok check
-if grep "Mesh OK" checkMesh.log; then
+if grep "Mesh OK" checkMesh-nonortho.log; then
   #printf \\n%s\\n "Mesh OK"
-  meshOk=0
-else
+  meshOk=0;
+
+exit 0
+else  #if the mesh is not ok, it searches if the problem is orthongolality or skweness, or both
   printf \\n%s\\n "Mesh non OK"
-  meshOk=1
-	if grep "Non-orthogonality check OK" checkMesh.log; then
+  meshOk=1;
+	if grep "Non-orthogonality check OK" $file; then
 	nonOrtho=0;
 	else
-	nonOrtho=1;
-	#max value
-	#ave value
-	prinft \\n%s\\n "Non ortho problematic"
-	#skew to do
+	nonOrtho=1; #it searches the max ortho
+	grep "Mesh non-orthogonality" $file> ortho.log
+	aveortho=$(awk '{print $NF}' ortho.log)
+	maxortho=$(awk '{print $4}' ortho.log)
+	echo ave ortho $aveortho
+	echo max ortho $maxortho
+	echo "Non ortho problematic"
 	fi
 
-grep "Max skewness" checkMesh.log > skew.log
+grep "Max skewness" $file > skew.log
 
 	if grep "OK" skew.log; then
 	nskew=0;
 	else
 	skew=1;
-	prinft \\n%s\\n "Skew problematic"
+	echo "Skew problematic"
+	awk '{print $4}' skew.log > temp.log
+	maxskew=$(sed s/,// temp.log)
+	echo max skew $maxskew
 	fi
 
 fi
